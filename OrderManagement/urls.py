334a2cc87f305eadb6d4e084f6ec807c755a808f
Adnan Jihad <jucse28.374@gmail.com>
2024-02-14 23:13:33 +0600

    ordermanagement initial commit

--- OrderManagement/urls.py
+++ OrderManagement/urls.py
@@ -5,4 +5,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
-urlpatterns = []
+urlpatterns = [
+    path('payment/<int:id>/<int:id2>/',views.payment,name='payment'),
+    path('<int:id>/',views.order,name='order'),
+    path('receipt/<int:id>/',views.receipt,name='receipt')
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- OrderManagement/views.py
+++ OrderManagement/views.py
@@ -1,3 +1,52 @@
+import re
+import json
+import datetime
+from decimal import Decimal
+from urllib import response
+from io import BytesIO
+from django.utils.html import strip_tags
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
-
-# Create your views here.
+from django.template.loader import get_template,render_to_string
+from django.views import View
+from xhtml2pdf import pisa
+from django.core.mail import send_mail,EmailMessage
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.db import IntegrityError
+from django.contrib import messages
+import json
+import random
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import Group,User
+from .models import *
+from Search.models import *
+from django.contrib.auth.decorators import login_required
+from Cart.models import *
+from Admin.models import *
+def order(request,id):
+    order=Order.objects.get(orderId=int(id))
+    if request.POST.get('pay')=='1':
+        redirectUrl='/order/payment/'+str(id)+'/'+str(1)
+        return redirect(redirectUrl)
+    if request.POST.get('pay')=='2':
+        redirectUrl='/order/payment/'+str(id)+'/'+str(2)
+        return redirect(redirectUrl)
+    if request.POST.get('pay')=='3':
+        redirectUrl='/order/payment/'+str(id)+'/'+str(3)
+        return redirect(redirectUrl)
+    if request.POST.get('pay')=='4':
+        return redirect('/cart')
+    context={
+        'order':order
+    }
+    return render(request,'order.html',context)
+def payment(request,id,id2):
+    order=Order.objects.get(orderId=int(id2))
+    context={
+        'id':id,
+        'order':order
+    }
+    return render(request,'payment.html',context)
