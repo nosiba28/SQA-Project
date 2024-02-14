6a8c604b4f2dd3c3420e819473c87a83619405f3
nosiba28 <jucse28.353@gmail.com>
2024-02-14 23:50:35 +0600

    documentation added for cart views

--- Cart/views.py
+++ Cart/views.py
@@ -28,26 +28,48 @@ from django.shortcuts import redirect
 from .models import indOrder
 
 # Create your views here.
+# Cart function:
 def cart(request):
+
+    """
+    View function for the cart page.
+
+    This function handles the rendering of the cart page, including updating
+    the quantities and total price of items in the cart.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response with the rendered cart page.
+
+    """
+    # Getting the customer and order details
     customer=Customer.objects.get(email=request.user.email)
     order=Order.objects.get(customer=customer,status=0)
     allProduct=indOrder.objects.filter(order=order).order_by('product_id')
+
+    # Post request for updating the cart's item
     if 'order' in request.POST:
         requestList=request.POST.getlist('product')
         totalSum=0
+        # update the quantities and total price
         for o,i in zip(allProduct,requestList):
             prod=Product.objects.get(productId=o.product.productId)
             indOrde=indOrder.objects.get(order=order,product=prod)
-            if int(i)==0:
+           
+            # Deleting the item if quantity is zero
+            if int(i)==0: 
                 indOrde.delete()
             else:
                 indOrde.quantity=int(i)
                 indOrde.save()
-            totalSum+=(int(i)*prod.offerPrice)
+            totalSum+=(int(i)*prod.price)
+        #updating the total price of the order
         order.total=totalSum
         order.save()
-        redirectUrl='/order/'+str(order.orderId)
-        return redirect(redirectUrl)                         
+        redirectUrl='/order/'+str(order.orderId) 
+        return redirect(redirectUrl)                              
     context={
         'allProduct':allProduct,
         'order':order
