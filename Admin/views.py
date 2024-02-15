2839d9e3a962316b45be1716c214f3372257740f
Asmaul-Shahana <asmaulshahana@gmail.com>
2024-02-15 06:45:17 +0600

    aadmin view added

--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+# CS: the first parameter in a view function should be called request.
+
+# admin view method
+
+def aadmin(request,id):
+    if 'ok' in request.POST:
+        redirectUrl=""
+        if request.POST.get('manage')=='1':
+            redirectUrl="/aadmin/addProduct/"+str(id)
+            return redirect(redirectUrl)
+        if request.POST.get('manage')=='2':
+            redirectUrl='/aadmin/updateProduct/'+str(id)
+            return redirect(redirectUrl)
+        if request.POST.get('manage')=='3':
+            redirectUrl='/aadmin/removeProduct/'+str(id)
+            return redirect(redirectUrl)
+        if request.POST.get('manage')=='4':
+            redirectUrl='/aadmin/allProduct/'+str(id)
+            return redirect(redirectUrl)
+    return render(request,'aadmin.html')
