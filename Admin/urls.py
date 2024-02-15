dfe206df706f80d18c1362aa80c39f2803fe6dc8
Asmaul-Shahana <asmaulshahana@gmail.com>
2024-02-15 06:06:36 +0600

    admin url added

--- Admin/urls.py
+++ Admin/urls.py
@@ -5,4 +5,12 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
-urlpatterns = []
+
+urlpatterns = [
+    path('<int:id>/',views.aadmin,name='aadmin'),
+    path('allProduct/<int:id>/',views.allProduct,name='allProduct'),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
