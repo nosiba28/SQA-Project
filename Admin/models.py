a51eec1ca598767076a42c11a2fc8e2e229868d0
Asmaul-Shahana <asmaulshahana@gmail.com>
2024-02-16 20:38:56 +0600

    more comments added

--- Admin/models.py
+++ Admin/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 
 # Creating Category model
-
 class Category(models.Model):
-    categoryName=models.CharField(max_length=100,blank=True,null=True)
+    """
+    Model for product category
+    """
+
+    # Define categoryName field for Category model
+    categoryName = models.CharField(max_length=100, blank=True, null=True)
+
+    # Return string representation of the Category object
     def __str__(self):
         return str(self.categoryName) 
 
 # Creating Product model
-
 class Product(models.Model):
-    productId=models.IntegerField(default=0,blank=True,null=True)
-    name=models.CharField(max_length=40,blank=True,null=True)
-    image=models.ImageField(upload_to='image/' ,blank=True,null=True)
-    desc=models.CharField(max_length=100,blank=True,null=True,default=None)
-    price=models.IntegerField(default=0,blank=True,null=True)
+    """
+    Model for product
+    """
+
+    # Define productId field for Product model
+    productId = models.IntegerField(default=0, blank=True, null=True)
+    # Define name field for Product model
+    name = models.CharField(max_length=40, blank=True, null=True)
+    # Define image field for Product model
+    image = models.ImageField(upload_to='image/', blank=True, null=True)
+    # Define desc field for Product model
+    desc = models.CharField(max_length=100, blank=True, null=True, default=None)
+    # Define price field for Product model
+    price = models.IntegerField(default=0, blank=True, null=True)
+
+    # Return string representation of the Product object
     def __str__(self):
-        return str(self.productId)+" - "+str(self.shop.shopId)   
+        return str(self.productId) + " - " + str(self.shop.shopId)
--- Admin/urls.py
+++ Admin/urls.py
@@ -1,19 +1,26 @@
 from django.urls import path
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 
+# Define URL patterns for the admin app
 urlpatterns = [
-    path('<int:id>/',views.aadmin,name='aadmin'),
-    path('addProduct/<int:id>/',views.addProduct,name='addProduct'),
-    path('updateProduct/<int:id>/',views.updateProduct,name='updateProduct'),
-    path('removeProduct/<int:id>/',views.removeProduct,name='removeProduct'),
-    path('allProduct/<int:id>/',views.allProduct,name='allProduct'),
-
+    # Path for accessing the aadmin view with shop ID as parameter
+    path('<int:id>/', views.aadmin, name='aadmin'),
+    # Path for adding a product with shop ID as parameter
+    path('addProduct/<int:id>/', views.addProduct, name='addProduct'),
+    # Path for updating a product with product ID as parameter
+    path('updateProduct/<int:id>/', views.updateProduct, name='updateProduct'),
+    # Path for removing a product with product ID as parameter
+    path('removeProduct/<int:id>/', views.removeProduct, name='removeProduct'),
+    # Path for viewing all products with shop ID as parameter
+    path('allProduct/<int:id>/', views.allProduct, name='allProduct'),
 ]
 
+# Add URL patterns for serving static files during development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Add URL patterns for serving media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
