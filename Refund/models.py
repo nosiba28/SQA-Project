50edd7d977b016ba575b2d49867047e4e97b50d3
Asmaul-Shahana <asmaulshahana@gmail.com>
2024-02-26 17:12:10 +0600

    Manage refund feature added

--- Refund/models.py
+++ Refund/models.py
@@ -1,10 +1,65 @@
-from django.db import models
+from django.shortcuts import render, redirect
+from .models import *  # Importing models from the same app
+from Admin.models import *  # Importing models from the Admin app
+from Cart.models import *   # Importing models from the Cart app
 
-# Create your models here.
+# RefundRequest model definition
 class RefundRequest(models.Model):
-    reason=models.CharField(max_length=100,null=True)
-    product=models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
-    order=models.ForeignKey(Order,on_delete=models.CASCADE,null=True)
-    status=models.IntegerField(default=0)
+    reason = models.CharField(max_length=100, null=True)  # Reason for the refund request
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)  # Product associated with the refund request
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)  # Order associated with the refund request
+    status = models.IntegerField(default=0)  # Status of the refund request
+
     def __str__(self):
-        return str(self.order.orderId)+" - "+str(self.product.productId) 
+        # String representation of the RefundRequest instance
+        return str(self.order.orderId) + " - " + str(self.product.productId)
+
+
+# View function for managing refund requests
+def manageRefund(request):
+    # Retrieve the owner object based on the current user's email
+    owner = Owner.objects.get(email=request.user.email)
+    
+    # Filter refund requests for products belonging to the shop owned by the current user
+    refunds = RefundRequest.objects.filter(product__shop=owner, status=0)
+    
+    # Process form submissions
+    if 'reject' in request.POST:
+        # Retrieve order and product based on form data
+        order = Order.objects.get(orderId=int(request.POST.get('order')))
+        product = Product.objects.get(productId=int(request.POST.get('product')))
+        
+        # Update refund request and associated order status
+        requestRefund = RefundRequest.objects.get(order=order, product=product)
+        requestRefund.status = 1
+        requestRefund.save()
+        refundOrder = indOrder.objects.get(order=order, product=product)
+        refundOrder.status = 3
+        refundOrder.save()
+        
+        # Redirect to the refund management page after processing
+        return redirect('/refund/manage')
+    
+    if 'accept' in request.POST:
+        # Retrieve order and product based on form data
+        order = Order.objects.get(orderId=int(request.POST.get('order')))
+        product = Product.objects.get(productId=int(request.POST.get('product')))
+        
+        # Update refund request and associated order status
+        requestRefund = RefundRequest.objects.get(order=order, product=product)
+        requestRefund.status = 1
+        requestRefund.save()
+        refundOrder = indOrder.objects.get(order=order, product=product)
+        refundOrder.status = 2
+        refundOrder.save()
+        
+        # Redirect to the refund management page after processing
+        return redirect('/refund/manage')
+    
+    # Prepare context to render the template
+    context = {
+        'refunds': refunds
+    }
+    
+    # Render the manageRefund.html template with the context data
+    return render(request, 'manageRefund.html', context)
--- Refund/views.py
+++ Refund/views.py
@@ -1,4 +1,3 @@
-
 import re
 import json
 import datetime
@@ -8,10 +7,10 @@ from io import BytesIO
 from django.utils.html import strip_tags
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
-from django.template.loader import get_template,render_to_string
+from django.template.loader import get_template, render_to_string
 from django.views import View
 from xhtml2pdf import pisa
-from django.core.mail import send_mail,EmailMessage
+from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -20,11 +19,61 @@ from django.db import IntegrityError
 from django.contrib import messages
 import json
 import random
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.models import Group,User
-from .models import *
-from Search.models import *
-from Cart.models import *
-from Wishlist.models import *
-from Admin.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group, User
+from .models import *  # Importing models from the current app
+from Search.models import *  # Importing models from the Search app
+from Cart.models import *  # Importing models from the Cart app
+from Wishlist.models import *  # Importing models from the Wishlist app
+from Admin.models import *  # Importing models from the Admin app
 from django.contrib.auth.decorators import login_required
+
+# View function for managing refund requests
+@login_required
+def manageRefund(request):
+    # Retrieve the owner object based on the current user's email
+    owner = Owner.objects.get(email=request.user.email)
+    
+    # Retrieve refund requests related to the owner's shop
+    refunds = RefundRequest.objects.filter(product__shop=owner, status=0)
+    
+    # Process form submissions
+    if 'reject' in request.POST:
+        # Retrieve order and product based on form data
+        order = Order.objects.get(orderId=int(request.POST.get('order')))
+        product = Product.objects.get(productId=int(request.POST.get('product')))
+        
+        # Update refund request and associated order status for rejection
+        requestRefund = RefundRequest.objects.get(order=order, product=product)
+        requestRefund.status = 1
+        requestRefund.save()
+        refundOrder = indOrder.objects.get(order=order, product=product)
+        refundOrder.status = 3
+        refundOrder.save()
+        
+        # Redirect to the refund management page after processing
+        return redirect('/refund/manage')
+    
+    if 'accept' in request.POST:
+        # Retrieve order and product based on form data
+        order = Order.objects.get(orderId=int(request.POST.get('order')))
+        product = Product.objects.get(productId=int(request.POST.get('product')))
+        
+        # Update refund request and associated order status for acceptance
+        requestRefund = RefundRequest.objects.get(order=order, product=product)
+        requestRefund.status = 1
+        requestRefund.save()
+        refundOrder = indOrder.objects.get(order=order, product=product)
+        refundOrder.status = 2
+        refundOrder.save()
+        
+        # Redirect to the refund management page after processing
+        return redirect('/refund/manage')
+    
+    # Prepare context to render the template
+    context = {
+        'refunds': refunds
+    }
+    
+    # Render the manageRefund.html template with the context data
+    return render(request, 'manageRefund.html', context)
