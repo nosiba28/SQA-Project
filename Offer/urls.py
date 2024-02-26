6b25d097107cfe373fac9c3eb01c96b550002864
nosiba28 <jucse28.353@gmail.com>
2024-02-26 19:24:58 +0600

    documentation added for Offer's urls

--- Offer/urls.py
+++ Offer/urls.py
@@ -5,6 +5,22 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
+
+"""
+    URL patterns for the application.
+
+    URL Configuration Commands:
+    - Directs requests to the 'offer' view.
+
+    Parameters:
+        path (str): The URL path to match.
+        views.offer (function): The view function to be called.
+        name (str): The name used to identify the URL pattern.
+
+    Returns:
+        list: List of URL patterns.
+"""
+
 urlpatterns = [
     path('',views.offer,name='offer'),
 ]
